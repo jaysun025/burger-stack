fd07e1655d06a22f77b3135a65bb11dd980c429a
tkadima <kadima.t@husky.neu.edu>
2021-02-08 22:08:53 -0500

    redid app.js to use hooks

--- src/App.js
+++ src/App.js
@@ -1,61 +1,48 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import IngredientList from './IngredientList'
 import BurgerPane from './BurgerPane'
 
+function App() {
 
-const ingredients = [
-    {name: 'Kaiser Bun', color: 'saddlebrown'},
-    {name: 'Sesame Bun', color: 'sandybrown'},
-    {name: 'Gluten Free Bun', color: 'peru'},
-    {name: 'Lettuce Wrap', color: 'olivedrab'},
-    {name: 'Beef Patty', color: '#3F250B'},
-    {name: 'Soy Patty', color: '#3F250B'},
-    {name: 'Black Bean Patty', color: '#3F250B'},
-    {name: 'Chicken Patty', color: 'burlywood'},
-    {name: 'Lettuce', color: 'lawngreen'},
-    {name: 'Tomato', color: 'tomato'},
-    {name: 'Bacon', color: 'maroon'},
-    {name: 'Onion', color: 'lightyellow'}
-  ]
+    const [ingredientList, setIngredientList] = useState([
+        {name: 'Kaiser Bun', color: 'saddlebrown'},
+        {name: 'Sesame Bun', color: 'sandybrown'},
+        {name: 'Gluten Free Bun', color: 'peru'},
+        {name: 'Lettuce Wrap', color: 'olivedrab'},
+        {name: 'Beef Patty', color: '#3F250B'},
+        {name: 'Soy Patty', color: '#3F250B'},
+        {name: 'Black Bean Patty', color: '#3F250B'},
+        {name: 'Chicken Patty', color: 'burlywood'},
+        {name: 'Lettuce', color: 'lawngreen'},
+        {name: 'Tomato', color: 'tomato'},
+        {name: 'Bacon', color: 'maroon'},
+        {name: 'Onion', color: 'lightyellow'}
+      ])
+  
+    const [burgerStack, setBurgerStack] = useState([])
 
+      console.log(ingredientList)
 
-class App extends Component{
-    state = {
-        burgerIngredients: []
+      
+
+    const addToBurger = (e) => {
+        setIngredientList(ingredientList.name, ingredientList.color)
     }
 
-    addToBurger = (e) => {
-        let currentBurger = this.state.burgerIngredients
-        // craete an object with name and color properties that collect vals from 
-        // e.target
-        let newIngredient = {name: e.target.innerText, color: e.target.style.backgroundColor}
-        // push object to current burger array
-        currentBurger.push(newIngredient)
-        // set state of burger ingredients to currentBurger
-
-
-       
-
-        this.setState({
-            burgerIngredients: currentBurger
-        })
+    const clearBurger = (e) => {
+        setBurgerStack([])
     }
 
-    clearBurger = (e) => {
-        this.setState({
-            burgerIngredients: []
-        })
-    }
-
-    render() {
+    
         return (
             <div style={{display: 'flex'}}>
-                <IngredientList ingredients={ingredients} addToBurger={this.addToBurger} />
-                <BurgerPane burgerIngredients={this.state.burgerIngredients} clearBurger={this.clearBurger} />
+                <IngredientList ingredients={ingredientList} addToBurger={addToBurger} />
+                <BurgerPane burgerStack={burgerStack} clearBurger={clearBurger} />
             </div>
         )
     }
-}    
+
+  
 
 
 export default App;
